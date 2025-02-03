555b2e9442863a2ffa3918dff30dd31673e86b30
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2025-02-04 00:19:37 +0530

    Update Minimun_Cost_To_Connect_Rods.cpp

--- Heap/Minimun_Cost_To_Connect_Rods.cpp
+++ Heap/Minimun_Cost_To_Connect_Rods.cpp
@@ -6,22 +6,24 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-        int n,k;
-        cin>>n>>k;
-        int tn=n;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        while(n--){
-            int a;
-            cin>>a;
-            pq.push(a);
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        priority_queue<int,vector<int>,greater<int>> mn;
+        
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            mn.push(a[i]);
         }
-        int la;
-        while(k>0){
-            la=pq.top();
-            pq.pop();
-            k--;
+        int c=0;
+        while(mn.size()>1){
+            int a=mn.top();mn.pop();
+            int b=mn.top();mn.pop();
+            c+=a+b;
+            mn.push(a+b);
         }
-        cout<<la<<endl;
+        cout<<c<<endl;
+        
     }
     return 0;
 }
